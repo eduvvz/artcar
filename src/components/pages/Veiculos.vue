63e0332ff2f0f2cbc6c82fbd81810f2cf971daaf
Eduardo Cerqueira <eduardocslv@gmail.com>
2018-12-16 13:24:58 -0200

    structure

---
+++ src/components/pages/Veiculos.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>veiculos</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+import './assets/scss/styles.scss'
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
