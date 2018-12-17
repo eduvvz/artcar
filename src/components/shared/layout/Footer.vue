a1150b3e73b83a9e232517326b4b416b6dd76ab8
Eduardo Cerqueira <eduardocslv@gmail.com>
2018-12-17 01:11:46 -0200

    footer

--- src/components/shared/layout/Footer.vue
+++ src/components/shared/layout/Footer.vue
@@ -1,5 +1,21 @@
 <template>
     <b-container fluid class="bg-color-3 p-5">
-        <b-row></b-row>
+        <b-row>
+            <b-col sm="3" >
+                <h5 class="color-1">Localização</h5>
+                <p class="color-1">
+                    Av. Brasil, 1309 - Jardim Monte Cristo, Suzano - SP, 08670-010
+                </p>
+            </b-col>
+            <b-col sm="3">
+                <h5 class="color-1">Atendimento</h5>
+                <p class="color-1">
+                    It is a long established fact that a re distracted by the readable content of a page when looking at its layout. The p.
+                </p>
+            </b-col>
+            <b-col offset-lg="4" sm="2">
+                <img class="w-100" src="../../../assets/img/artcar-logo.png" alt="">
+            </b-col>
+        </b-row>
     </b-container>
 </template>
